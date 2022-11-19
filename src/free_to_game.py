f41b5ec7ec3457211994655aad0dd89fd7d346c9
ecclesias <77536370+zeviel@users.noreply.github.com>
2022-11-19 20:05:40 +0800

    Update free_to_game.py

--- src/free_to_game.py
+++ src/free_to_game.py
@@ -1,25 +1,32 @@
 from requests import get
 
 class FreeToGame:
-	def __init__(self):
+	def __init__(self) -> None:
 		self.api = "https://www.freetogame.com/api"
 		self.headers = {
 			"User-Agent": "Mozilla/5.0 (Linux; Android 11; RMX2086 Build/RKQ1.200928.002; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/101.0.4951.61 Mobile Safari/537.36"
 		}
 	
-	def get_live_games(self):
-		return get(f"{self.api}/games", headers=self.headers).json()
+	def get_live_games(self) -> dict:
+		return get(
+			f"{self.api}/games",
+			headers=self.headers).json()
 	
-	def get_games_by_platform(self, platform: str = "pc"):
+	def get_games_by_platform(
+			self,
+			platform: str = "pc") -> dict:
 		return get(
 			f"{self.api}/games?platform={platform}",
 			headers=self.headers).json()
 	
-	def get_games_by_category(self, category: str = "shooter"):
+	def get_games_by_category(
+			self,
+			category: str = "shooter") -> dict:
 		return get(
-			f"{self.api}/games?category={category}", headers=self.headers).json()
+			f"{self.api}/games?category={category}",
+			headers=self.headers).json()
 	
-	def sort_games(self, sort: str):
+	def sort_games(self, sort: str) -> dict:
 		return get(
 			f"{self.api}/games?sort-by={sort}",
 			headers=self.headers).json()
@@ -28,12 +35,12 @@ class FreeToGame:
 			self,
 			platform: str = "browser",
 			category: str = "mmorpg",
-			sort: str = "release-date"):
+			sort: str = "release-date") -> dict:
 		return get(
 			f"{self.api}/games?platform={platform}&category={category}&sort-by={sort}",
 			headers=self.headers).json()
 		
-	def get_game_details(self, game_id: int):
+	def get_game_details(self, game_id: int) -> dict:
 		return get(
 			f"{self.api}/game?id={game_id}",
 			headers=self.headers).json()
@@ -41,7 +48,7 @@ class FreeToGame:
 	def filter_game(
 			self,
 			tag: str = "3d.mmorpg.fantasy.pvp",
-			platform: str = "pc"):
+			platform: str = "pc") -> dict:
 		return get(
 			f"{self.api}/filter?tag={tag}&platform={platform}",
 			headers=self.headers).json()
